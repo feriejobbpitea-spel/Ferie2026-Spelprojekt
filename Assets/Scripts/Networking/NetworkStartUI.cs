a2d421bad9df028b91fc51153878920ce4171f02
ISAK <Kaleido>
2026-06-17 10:38:48 +0200

    no message

--- Assets/Scripts/Networking/NetworkStartUI.cs
+++ Assets/Scripts/Networking/NetworkStartUI.cs
@@ -1,26 +1,88 @@
+using Blocks.Sessions;
+using Blocks.Sessions.Common;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
 using Unity.Netcode;
+using Unity.Properties;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
-/// Skapar en tillfällig UI för att hosta eller joina ett spel
+/// Hanterar UI att hosta eller joina ett spel
 /// </summary>
 public class NetworkStartUI : MonoBehaviour
 {
-    private void OnGUI()
+    [SerializeField] private QuickJoinViewModel m_ViewModel;
+    [SerializeField] private QuickJoinSettings QuickJoinSettings;
+    [SerializeField] private SessionSettings m_SessionSettings;
+
+    [SerializeField] private GameObject LoadingScreen;
+
+    public SessionSettings SessionSettings
     {
-        float w = 200f, h = 40f;
-        float x = 10f, y = 10f;
-
-        if (NetworkManager.Singleton == null)
-            return;
-
-        if(!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        get => m_SessionSettings;
+        set
         {
-            if (GUI.Button(new Rect(x, y, w, h), "Start Client"))
-                NetworkManager.Singleton.StartClient();
+            if (m_SessionSettings == value)
+            {
+                return;
+            }
 
-            if (GUI.Button(new Rect(x, y + h, w, h), "Start Host"))
-                NetworkManager.Singleton.StartHost();
+            m_SessionSettings = value;
         }
+    }
+
+    [SerializeField] private ButtonWrapper quickJoinButton;
+    
+    private void Awake()
+    {
+        LoadingScreen.SetActive(false);
+
+        m_ViewModel = new QuickJoinViewModel(m_SessionSettings?.sessionType);
+        SessionSettings = m_SessionSettings;
+    }
+
+    private void OnEnable()
+    {
+        quickJoinButton.onClick.AddListener(OnQuickJoinClicked);
+    }
+
+    private void OnDisable()
+    {
+        quickJoinButton.onClick.RemoveListener(OnQuickJoinClicked);
+    }
+
+    private void Update()
+    {
+        quickJoinButton.interactable = m_ViewModel.CanClickButton;
+    }
+
+    private void OnQuickJoinClicked()
+    {
+
+        if (!SessionSettings)
+        {
+            Debug.LogError("SessionSettings is null, it needs to be assigned.");
+            return;
+        }
+        if (!m_ViewModel.AreMultiplayerServicesInitialized())
+        {
+            Debug.LogError("Multiplayer Services are not initialized. You can initialize them with default settings by adding a ServicesInitialization and PlayerAuthentication components in your scene.");
+            return;
+        }
+
+        Task joiningTask = m_ViewModel.MatchmakeSessionAsync(QuickJoinSettings.ToQuickJoinOptions(), m_SessionSettings.ToSessionOptions());
+        StartCoroutine(JoiningGame(joiningTask));
+    }
+
+    private IEnumerator JoiningGame(Task joiningTask) 
+    {
+        while(!joiningTask.IsCompleted)
+        {
+            LoadingScreen.SetActive(true);
+            yield return null;
+        }
+        LoadingScreen.SetActive(false);
     }
 }
